56f7201a32134c25c4d9297522f5ddc2ee1948d6
Eduardo Moreno <emoreno911@gmail.com>
2024-12-24 15:17:22 -0400

    quick fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import GameContainer from '@/components/Common/GameContainer';
 import GameButton from '@/components/Common/GameButton';
 import AnimatedLogo from '@/components/Common/AnimatedLogo';
 import { IconMusic, IconTrophy, IconUser, IconWallet } from '@tabler/icons-react';
-import AudioAnalyzer from '@/components/Common/AudioAnalyzer';
 
 
 export default function Home() {
@@ -30,11 +29,7 @@ export default function Home() {
           </div>
           <h2 className="font-gaming font-semibold text-pink-600 text-5xl text-shadow-black z-10 home-title-margin">Guess the Beat</h2>
           <h3 className="font-bold text-xl font-cursive text-center text-sky-500 text-shadow-black mb-4 z-10 px-4 max-w-96">Test your music knowledge and become a Tune Master!</h3>
-          {/* <div className="rounded-full w-48 h-48 z-10">
-            <img src={d.photoURL} alt="app-pic" className="" />
-          </div> */}
           
-
           <Link href="/music-game">
             <GameButton className="!min-w-80">
               {/* <IconMusic /> */}
