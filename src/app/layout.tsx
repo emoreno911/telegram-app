d5b9f7f0abc92999d1b7e81abcf5812f782c7b89
Eduardo Moreno <emoreno911@gmail.com>
2024-12-29 17:24:49 -0400

    fix web metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,11 +11,21 @@ import "@telegram-apps/telegram-ui/dist/styles.css";
 import "normalize.css/normalize.css";
 import "./_assets/globals.css";
 import "./_assets/custom.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Guess The Beat",
   description: "The Telegram game for music lovers",
-  icons: "https://gateway.pinata.cloud/ipfs/bafkreiahcoxixyhzjk7fivigz2sov4rvrygb5wkpvzkg3juy3zixp3d6vm"
+  openGraph: {
+    type: "website",
+    url: "https://guessthebeat.xyz",
+    title: "Guess The Beat",
+    description: "The Telegram game for music lovers",
+    siteName: "Guess The Beat",
+    images: [{
+      url: "https://gateway.pinata.cloud/ipfs/bafkreiahcoxixyhzjk7fivigz2sov4rvrygb5wkpvzkg3juy3zixp3d6vm",
+    }],
+  }
 };
 
 export default async function RootLayout({ children }: PropsWithChildren) {
@@ -23,6 +33,12 @@ export default async function RootLayout({ children }: PropsWithChildren) {
 
   return (
     <html lang={locale}>
+      <link
+        rel="icon"
+        href="/favico.ico"
+        type="image/x-icon"
+        sizes="any"
+      />
       <body>
         <I18nProvider>
           <Root>
